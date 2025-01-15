04ca806e4c87948088af76a41d1adaf9718de541
Gyuheon Oh <goh@Gyuheons-MacBook-Pro.local>
2025-01-14 23:37:11 -0600

    fix express server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
 const app: Application = express();
@@ -6,6 +6,13 @@ const app: Application = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+// Root Route
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the Express server!');
+});
+
 
 // Routes
 import apiRoutes from './routes/api';
