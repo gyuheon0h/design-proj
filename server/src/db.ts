7ddcce3f0ba951d54ec41084726683f443bacf57
Gyuheon Oh <goh@Gyuheons-MacBook-Pro.local>
2025-01-27 16:35:40 -0600

    Postgres startup

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,1 +1,27 @@
-// postgres access
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.POSTGRES_ID,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432, // Default PostgreSQL port
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the PostgreSQL database.');
+});
+
+pool.on('error', (err) => {
+  console.error('Error connecting to the PostgreSQL database:', err);
+});
+
+export const query = (text: string, params?: any[]) => {
+  return pool.query(text, params);
+};
+
+export default pool;
