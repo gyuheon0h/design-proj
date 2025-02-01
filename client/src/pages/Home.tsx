7f816dc9370befa1d136f9c529b72a314d1cc1be
jakelehrman <90657833+jakelehrman@users.noreply.github.com>
2025-01-31 21:12:42 -0600

    Folder Component (#43)
    * Folder Component
    
    * padding for divider
    
    ---------
    
    Co-authored-by: Jake Lehrman <jake.lehrman@mastersny.org>
    Co-authored-by: Ethan H. <84256987+itszapenguin@users.noreply.github.com>
    Co-authored-by: Ethan Hsu <itszapenguin@gmail.com>

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Folder from '../components/Folder';
 import Divider from '@mui/material/Divider';
 import SearchBar from '../components/SearchBar';
 import FileContainer from '../components/FileContainer';
@@ -9,8 +10,17 @@ const Home = () => {
       <h1>Your File Storage:</h1>
       <SearchBar location="Storage" />
       <h2>Folders</h2>
+      <Folder
+        id="id"
+        name="Folder"
+        owner="Jake"
+        createdAt={new Date('2025-01-31')}
+        parentFolder={null}
+        folderChildren={[]}
+        fileChildren={[]}
+      />
       {/* <FileContainer></FileContainer> */}
-      <Divider></Divider>
+      <Divider style={{ padding: '20px' }}></Divider>
       <h2>Files</h2>
       {/* <FolderContainer></FolderContainer> */}
     </div>
--- client/src/pages/Shared.tsx
+++ client/src/pages/Shared.tsx
@@ -7,7 +7,7 @@ const Shared = () => {
       <h1>Shared With Me:</h1>
       <SearchBar location="Shared" />
       <h2>Folders</h2>
-      <Divider></Divider>
+      <Divider style={{ padding: '20px' }}></Divider>
       <h2>Files</h2>
     </div>
   );
