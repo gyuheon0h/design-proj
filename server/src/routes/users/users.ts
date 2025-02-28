8aba3f2668122025cd72ee87feead17a8a4c3cb8
emily <emilyx_yang@live.com>
2025-02-28 02:07:25 -0600

    updated favorites to use permission model and updated routes

--- server/src/routes/users/users.ts
+++ server/src/routes/users/users.ts
@@ -146,8 +146,8 @@ userRouter.get(
 );
 
 /**
- * GET /api/files/favorites/:ownerId
- * Route to get favorited files owned by a certain user (ownerId).
+ * GET /api/:userId/favorites/folder
+ * Route to get favorited folders (including shared).
  * This is protected by authorize
  */
 
@@ -161,23 +161,54 @@ userRouter.get(
       }
       const userId = req.user.userId;
 
-      const favoritedFolders = await FolderModel.getAllByOwnerAndColumn(
-        userId,
-        'isFavorited',
-        true,
+      const permissions = await PermissionModel.getFoldersByUserId(userId);
+      const favoritedFolders = await Promise.all(
+        permissions.filter(perm => perm.isFavorited === true)
+        .map((perm) => FolderModel.getById(perm.fileId)),
       );
+
       return res.json(favoritedFolders);
     } catch (error) {
-      console.error('Error getting files by owner:', error);
+      console.error('Error getting folders favorited by user:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   },
 );
 
 /**
- * GET /api/files/favorites/
- * Route to get favorited files owned by a certain user (ownerId).
- * This is protected by authorize
+ * GET /api/user/permissions/:fileId
+ * Route to get the permission for the user for the individual folder or file
+ * (mainly used to access the isFavorited status).
+ * 
+ * Note: you don't need :userId because it's implicitly passed in through the session token,
+ * unless we want it to be consistent with the other routes.
+ */
+
+userRouter.get(
+  "/permissions/:fileId",
+  authorize, 
+  async (req: AuthenticatedRequest, res) => {
+    try {
+      if (!req.user) {
+        return res.status(401).json({error: "Unauthorized"});
+      }
+
+      const userId = req.user.userId;
+      const { fileId } = req.params;
+
+      const permission = await PermissionModel.getPermissionByFileAndUser(fileId, userId);
+      return res.json(permission);
+    } catch (error) {
+      console.error("Error getting permission by user and file: ", error);
+      return res.status(500).json({error: "Internal Service Error"});
+    }
+  },
+);
+
+
+/**
+ * GET /api/:userId/favorites/file
+ * Route to get [userId]'s favorite files (including shared files).
  */
 
 userRouter.get(
@@ -189,15 +220,43 @@ userRouter.get(
         return res.status(401).json({ error: 'Unauthorized' });
       }
       const userId = req.user.userId;
-
-      const favoritedFiles = await FileModel.getAllByOwnerAndColumn(
-        userId,
-        'isFavorited',
-        true,
+      
+      const permissions = await PermissionModel.getFilesByUserId(userId); // TODO: does this include files that you own? how does shared pages work then? does it filter those out?
+      const favoritedFiles = await Promise.all(
+        permissions.filter(perm => perm.isFavorited === true)
+        .map((perm) => FileModel.getById(perm.fileId)),
       );
+
       return res.json(favoritedFiles);
     } catch (error) {
-      console.error('Error getting files by owner:', error);
+      console.error('Error getting files favorited by user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+);
+
+/**
+ * PATCH /api/userId/favorites/:fileId
+ */
+userRouter.patch(
+  '/:userId/favorites/:fileId',
+  authorize,
+  async (req: AuthenticatedRequest, res) => {
+    try {
+      if (!req.user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      const userId = req.user.userId;
+      
+      const permissions = await PermissionModel.getFilesByUserId(userId); // TODO: does this include files that you own? how does shared pages work then? does it filter those out?
+      const favoritedFiles = await Promise.all(
+        permissions.filter(perm => perm.isFavorited === true)
+        .map((perm) => FileModel.getById(perm.fileId)),
+      );
+
+      return res.json(favoritedFiles);
+    } catch (error) {
+      console.error('Error getting files favorited by user:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   },
